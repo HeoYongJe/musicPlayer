24fd9cb4fb492c12ecd4e29b32e143b7dc4900e7
HeoYongJe <hyj.campus@gmail.com>
2020-05-15 21:29:07 +0900

    music js add

--- js/player.js
+++ js/player.js
@@ -6,8 +6,14 @@ const $prevBtn = document.getElementById('prevBtn');
 
 const $listBtn = document.querySelector('.listBtn');
 const $container = document.querySelector('.container');
-
-$listBtn.onclick = function(){
-    $listContainer.style.display = 'block'
-    $container.style.width = '800px'
-}
+const $listContainer = document.querySelector('.listContainer');
+const setListOpenStatus = (boolean) => {
+    if(Boolean) {
+        $container.style.width = '400px';
+        $listContainer.style.display = 'none';
+      } else {
+        $container.style.width = '800px';
+        $listContainer.style.display = 'block';
+      }
+    };
+// }
